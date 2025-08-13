e3b32e6c47ee3e715cb0a3ad12e2a3dfff2860cc
mishohan <mishohan.o786@gmail.com>
2025-08-13 18:23:30 +0800

    Second Commit

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // Etch - A - Sketch
 
+
+
+
+for(let i = 0; i < 16 ; i++){
+
+    for(let j = 0; j < 16 ; j++ ){
+    const horizontalDiv = document.createElement('div');
+    horizontalDiv.classList.add('inner');
+    document.querySelector('.container').appendChild(horizontalDiv);
+}
+}
